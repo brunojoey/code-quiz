6f19931ee79a76aa6cdffe5a712f512c116ac861
Joseph Bruno <brunojoey12@gmail.com>
2020-02-26 18:27:59 -0600

    Edited code for the start and next buttons. Contained them in a div. Started work on the CSS.

---
+++ script.js
@@ -0,0 +1,71 @@
+var startButton = document.querySelector("#start-btn");
+var nextButton = document.querySelector("#next-btn");
+var questionTextArea = document.querySelector(".question-area");
+var answersArea = document.querySelector(".answers-area");
+var answerOneButton = document.querySelector(".answer-one");
+var answerTwoButton = document.querySelector(".answer-two");
+var answerThreeButton = document.querySelector(".answer-three");
+var answerFourButton = document.querySelector(".answer-four");
+
+var questionsAnswers = [ 
+    { "question": "How do you select an item that contains an ID?",
+         "answers": [ {"Oakland Raiders": false}, {"Chicago Bears": false}, {"San Francisco 49ers": false}, {"Washington Redskins": true} ]
+    }, 
+
+    { "question": "How do you select an item that contains an ID?",
+         "answers": [ {"Oakland Raiders": false}, {"Chicago Bears": false}, {"San Francisco 49ers": false}, {"Washington Redskins": true} ]
+    }, 
+
+    { "question": "How do you select an item that contains an ID?",
+         "answers": [ {"Oakland Raiders": false}, {"Chicago Bears": false}, {"San Francisco 49ers": false}, {"Washington Redskins": true} ]
+    }, 
+
+    { "question": "How do you select an item that contains an ID?",
+         "answers": [ {"Oakland Raiders": false}, {"Chicago Bears": false}, {"San Francisco 49ers": false}, {"Washington Redskins": true} ]
+    }, 
+
+    { "question": "How do you select an item that contains an ID?",
+         "answers": [ {"Oakland Raiders": false}, {"Chicago Bears": false}, {"San Francisco 49ers": false}, {"Washington Redskins": true} ]
+    }, 
+
+    { "question": "How do you select an item that contains an ID?",
+         "answers": [ {"Oakland Raiders": false}, {"Chicago Bears": false}, {"San Francisco 49ers": false}, {"Washington Redskins": true} ]
+    }, 
+
+    { "question": "How do you select an item that contains an ID?",
+         "answers": [ {"Oakland Raiders": false}, {"Chicago Bears": false}, {"San Francisco 49ers": false}, {"Washington Redskins": true} ]
+    }, 
+
+    { "question": "How do you select an item that contains an ID?",
+         "answers": [ {"Oakland Raiders": false}, {"Chicago Bears": false}, {"San Francisco 49ers": false}, {"Washington Redskins": true} ]
+    }, 
+
+    { "question": "How do you select an item that contains an ID?",
+         "answers": [ {"Oakland Raiders": false}, {"Chicago Bears": false}, {"San Francisco 49ers": false}, {"Washington Redskins": true} ]
+    }, 
+
+    { "question": "How do you select an item that contains an ID?",
+         "answers": [ {"Oakland Raiders": false}, {"Chicago Bears": false}, {"San Francisco 49ers": false}, {"Washington Redskins": true} ]
+    }];
+
+var score = 0;
+var highScore = 0;
+var highScoreInitials = "";
+
+startButton.addEventListener("click", startGame);
+
+function startGame() {
+for (var i = 0; i < questionsAnswers.length; i++) {
+
+}    
+};
+
+nextButton.addEventListener("click", nextButton);
+
+function nextButton() {
+
+}
+
+function selectAnswer() {
+    
+}
