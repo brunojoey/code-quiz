e169b524de35084570b2db3c9a10bf0a2bae3349
Joseph Bruno <brunojoey12@gmail.com>
2020-02-27 22:30:50 -0600

    I added a timer interval to the Javascript to get the countdown timer of the quiz.

--- script.js
+++ script.js
@@ -5,156 +5,170 @@ var answerOneButton = document.querySelector(".answer-one");
 var answerTwoButton = document.querySelector(".answer-two");
 var answerThreeButton = document.querySelector(".answer-three");
 var answerFourButton = document.querySelector(".answer-four");
-// var allAnswerButtons = document.querySelector(".btn-answer");
-
-console.log(answerFourButton);
-console.log(answerOneButton);
-
-var questionsAnswers = [ 
-    { question: "How do you select an item that contains an ID?",
-         answers: [ 
-            answerOneButton = {"Oakland Raiders": false},
-            answerTwoButton = {"Chicago Bears": false}, 
-            answerThreeButton = {"San Francisco 49ers": false}, 
-            answerFourButton = {"Washington Redskins": true} 
-            ]
-    }, 
-
-    { question: "How do you select an item that contains an ID?",
-         answers: [ 
-            answerOneButton = {"Oakland Raiders": false},
-            answerTwoButton = {"Chicago Bears": false}, 
-            answerThreeButton = {"San Francisco 49ers": false}, 
-            answerFourButton = {"Washington Redskins": true} 
-            ]
-    }, 
-
-    { question: "How do you select an item that contains an ID?",
-         answers: [ 
-            answerOneButton = {"Oakland Raiders": false},
-            answerTwoButton = {"Chicago Bears": false}, 
-            answerThreeButton = {"San Francisco 49ers": false}, 
-            answerFourButton = {"Washington Redskins": true} 
-            ]
-    }, 
-
-    { question: "How do you select an item that contains an ID?",
-         answers: [ 
-            answerOneButton = {"Oakland Raiders": false},
-            answerTwoButton = {"Chicago Bears": false}, 
-            answerThreeButton = {"San Francisco 49ers": false}, 
-            answerFourButton = {"Washington Redskins": true} 
-            ]
-    }, 
-
-    { question: "How do you select an item that contains an ID?",
-         answers: [ 
-            answerOneButton = {"Oakland Raiders": false},
-            answerTwoButton = {"Chicago Bears": false}, 
-            answerThreeButton = {"San Francisco 49ers": false}, 
-            answerFourButton = {"Washington Redskins": true} 
-            ]
-    }, 
-
-    { question: "How do you select an item that contains an ID?",
-         answers: [ 
-            answerOneButton = {"Oakland Raiders": false},
-            answerTwoButton = {"Chicago Bears": false}, 
-            answerThreeButton = {"San Francisco 49ers": false}, 
-            answerFourButton = {"Washington Redskins": true} 
-            ]
-    }, 
-
-    { question: "How do you select an item that contains an ID?",
-         answers: [ 
-            answerOneButton = {"Oakland Raiders": false},
-            answerTwoButton = {"Chicago Bears": false}, 
-            answerThreeButton = {"San Francisco 49ers": false}, 
-            answerFourButton = {"Washington Redskins": true} 
-            ]
-    }, 
-
-    { question: "How do you select an item that contains an ID?",
-         answers: [ 
-            answerOneButton = {"Oakland Raiders": false},
-            answerTwoButton = {"Chicago Bears": false}, 
-            answerThreeButton = {"San Francisco 49ers": false}, 
-            answerFourButton = {"Washington Redskins": true} 
-            ]
-    }, 
-
-    { question: "How do you select an item that contains an ID?",
-         answers: [ 
-            answerOneButton = {"Oakland Raiders": false},
-            answerTwoButton = {"Chicago Bears": false}, 
-            answerThreeButton = {"San Francisco 49ers": false}, 
-            answerFourButton = {"Washington Redskins": true} 
-            ]
-    }, 
-
-    { question: "How do you select an item that contains an ID?",
-         answers: [ 
-            answerOneButton = {"Oakland Raiders": false},
-            answerTwoButton = {"Chicago Bears": false}, 
-            answerThreeButton = {"San Francisco 49ers": false}, 
-            answerFourButton = {"Washington Redskins": true} 
-            ]
-    }];
-
+var allAnswerButtons = document.querySelector(".btn-answer");
 var score = 0;
 var highScore = 0;
 var highScoreInitials = "";
 var currentQuestion = 0;
+nextButton.style.display = "none";
+var answers = document.getElementById("answers-area");
+var timeEl = document.querySelector(".time-left");
+
+var questions = [ 
+    { 
+        question: "How do you select an item that contains an ID?",
+        answers: [ 
+            {label: "Oakland Raiders", value: false},
+            {label: "Chicago Bears", value: false}, 
+            {label: "San Francisco 49ers", value: false}, 
+            {label: "Washington Redskins", value: true},
+        ]
+    },
+    { 
+        question: "How do you select an item that contains an ID?",
+        answers: [ 
+            {label: "Oakland Raiders", value: false},
+            {label: "Chicago Bears", value: false}, 
+            {label: "San Francisco 49ers", value: false}, 
+            {label: "Washington Redskins", value: true},
+        ]
+    },
+    { 
+        question: "How do you select an item that contains an ID?",
+        answers: [ 
+            {label: "Oakland Raiders", value: false},
+            {label: "Chicago Bears", value: false}, 
+            {label: "San Francisco 49ers", value: false}, 
+            {label: "Washington Redskins", value: true},
+        ]
+    },
+    { 
+        question: "How do you select an item that contains an ID?",
+        answers: [ 
+            {label: "Oakland Raiders", value: false},
+            {label: "Chicago Bears", value: false}, 
+            {label: "San Francisco 49ers", value: false}, 
+            {label: "Washington Redskins", value: true},
+        ]
+    },
+    { 
+        question: "How do you select an item that contains an ID?",
+        answers: [ 
+            {label: "Oakland Raiders", value: false},
+            {label: "Chicago Bears", value: false}, 
+            {label: "San Francisco 49ers", value: false}, 
+            {label: "Washington Redskins", value: true},
+        ]
+    },
+    { 
+        question: "How do you select an item that contains an ID?",
+        answers: [ 
+            {label: "Oakland Raiders", value: false},
+            {label: "Chicago Bears", value: false}, 
+            {label: "San Francisco 49ers", value: false}, 
+            {label: "Washington Redskins", value: true},
+        ]
+    },
+    { 
+        question: "How do you select an item that contains an ID?",
+        answers: [ 
+            {label: "Oakland Raiders", value: false},
+            {label: "Chicago Bears", value: false}, 
+            {label: "San Francisco 49ers", value: false}, 
+            {label: "Washington Redskins", value: true},
+        ]
+    },
+    { 
+        question: "How do you select an item that contains an ID?",
+        answers: [ 
+            {label: "Oakland Raiders", value: false},
+            {label: "Chicago Bears", value: false}, 
+            {label: "San Francisco 49ers", value: false}, 
+            {label: "Washington Redskins", value: true},
+        ]
+    },
+    { 
+        question: "How do you select an item that contains an ID?",
+        answers: [ 
+            {label: "Oakland Raiders", value: false},
+            {label: "Chicago Bears", value: false}, 
+            {label: "San Francisco 49ers", value: false}, 
+            {label: "Washington Redskins", value: true},
+        ]
+    },
+    { 
+        question: "How do you select an item that contains an ID?",
+        answers: [ 
+            {label: "Oakland Raiders", value: false},
+            {label: "Chicago Bears", value: false}, 
+            {label: "San Francisco 49ers", value: false}, 
+            {label: "Washington Redskins", value: true},
+        ]
+    }];
 
 startButton.addEventListener("click", startGame);
 
 function startGame() {
+    setTime();
     startButton.style.display = "none";
-    var questionCurrent = questionsAnswers[0].question;
+    nextButton.style.display = "block";
+    var questionCurrent = questions[0].question;
+    questions[0].answers.forEach(function(answer) {
+        var button = document.createElement("button");
+        button.textContent = answer.label;
+        button.setAttribute("data-value", answer.value);
+        answers.appendChild(button);
+    });
     questionTextArea.innerHTML = "<p>" + questionCurrent + "<p>";
 };
 
-nextButton.addEventListener("click", nextButton);
+nextButton.addEventListener("click", nextQuestion);
 
-function nextButton() {
-
+function nextQuestion() {
+    
 }
 
-// allAnswerButtons.addEventListener("click", selectAnswer);
-// use this to get all buttons, attributes, and data.
+function setTime() {
+  var timerInterval = setInterval(function() {
+    var secondsLeft = 120;
+    secondsLeft--;
+    timeEl.textContent = secondsLeft + " seconds left";
 
-function selectAnswer() {
-    if (answerOneButton === false) {
-        answerIsWrong();
-    } else {
-        score++;
-        answerIsCorrect();
+    if(secondsLeft === 0) {
+      clearInterval(timerInterval);
+      sendMessage();
     }
-    if (answerTwoButton === false) {
-        answerIsWrong();
-    } else {
-        score++;
-        answerIsCorrect();
-    }
-    if (answerThreeButton === false) {
-        answerIsWrong();
-    } else {
-        score++;
-        answerIsCorrect();
-    }
-    if (answerFourButton === false) {
-        answerIsWrong();
-    } else {
-        score++;
-        answerIsCorrect();
+
+  }, 1000);
+}
+
+function sendMessage() {
+    
+}
+
+
+answers.addEventListener("click", selectAnswer);
+
+function selectAnswer(event) {
+    if (event.target.matches("button")) {
+        var selectedAnswer = event.target.getAttribute("data-value");
+        if (selectedAnswer === "true") {
+            score++;
+            secondsLeft--;
+            answerIsCorrect();
+            console.log("You're right!");
+        } else {
+            answerIsWrong();
+        }
     }
 }
 
 function answerIsCorrect(){
-    answerIsCorrect.style.backgroundColor = "#0f0";
+    questionTextArea.style.backgroundColor = "#0f0";
 }
 
 function answerIsWrong(){
-    answerIsWrong.style.backgroundColor = "#f00";
+    questionTextArea.style.backgroundColor = "#f00";
 }
+setTime();
 
